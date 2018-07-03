d65a5ede90123503a2a8490bd45ca80da46f1a80
Isaac Lapworth <isaacdavidlapworth@gmail.com>
2018-07-03 15:52:32 +0100

    Show "cannot access camera" correctly

--- src/components/WebcamCapture/WebcamCapture.js
+++ src/components/WebcamCapture/WebcamCapture.js
@@ -3,6 +3,10 @@ import './WebcamCapture.css';
 import Webcam from 'react-webcam';
 
 class WebcamCapture extends Component {
+	state = {
+		cameraConnected: false
+	};
+
 	constructor(props) {
 		super(props);
 		this.webcam = React.createRef();
@@ -13,32 +17,39 @@ class WebcamCapture extends Component {
 		return this.webcam.current.getScreenshot();
 	}
 
+	componentDidMount() {
+		navigator.mediaDevices
+			.getUserMedia({video: true})
+			.then(connected => this.setState({cameraConnected: true}))
+			.catch(err => this.setState({cameraConnected: false}));
+	}
+
 	render() {
-		const height = 400;
-		const width = 400;
+		if (this.state.cameraConnected) {
+			const height = 400;
+			const width = 400;
 
-		const videoConstraints = {
-			width,
-			height,
-			facingMode: 'user'
-		};
+			const videoConstraints = {
+				width,
+				height,
+				facingMode: 'user'
+			};
 
-		if (!this.webcam.current) return <div>Cannot access camera</div>;
-
-		return (
-			<div>
-				<Webcam
-					audio={false}
-					height={height}
-					ref={this.webcam}
-					screenshotFormat="image/jpeg"
-					width={width}
-					videoConstraints={videoConstraints}
-					className="videoStream"
-					screenshotWidth={224}
-				/>
-			</div>
-		);
+			return (
+				<div>
+					<Webcam
+						audio={false}
+						height={height}
+						ref={this.webcam}
+						screenshotFormat="image/jpeg"
+						width={width}
+						videoConstraints={videoConstraints}
+						className="videoStream"
+						screenshotWidth={224}
+					/>
+				</div>
+			);
+		} else return <div>Cannot access camera</div>;
 	}
 }
 
