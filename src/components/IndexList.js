85b28e592a27671160b68cef2df01cf97037570a
Isaac Lapworth <isaacdavidlapworth@gmail.com>
2018-07-03 08:32:57 +0100

    Add basic list keyed by index values

---
+++ src/components/IndexList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const IndexList = props => {
+	return(
+		<div>
+			<ul>{props.items.map((item, index) => <li key={index}>{item}</li>)}</ul>
+		</div>
+	);
+}
+
+export default IndexList;
