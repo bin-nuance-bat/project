f723f2984c46e0ea5de489110790a1b6d136da96
Isaac Lapworth <isaacdavidlapworth@gmail.com>
2018-07-03 08:25:56 +0100

    Add simple yes/no box

---
+++ src/components/ConfirmationBox.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const ConfirmationBox = props => {
+
+	return (
+		<div>
+			<div>
+				{props.text}
+			</div>
+			<div>
+				<button onClick={props.ifYes}>Yes</button>
+				<button onClick={props.ifNo}>No</button>
+			</div>
+		</div>
+	);
+}
+
+export default ConfirmationBox;
