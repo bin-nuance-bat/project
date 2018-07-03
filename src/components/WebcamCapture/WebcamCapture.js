e63452d5702a4e5582569ac08232362d8bb547f6
Gurveer Arora <Garora1410@yahoo.co.uk>
2018-07-03 10:52:07 +0100

    Video display (#15)
    * Add component to capture and display video feed
    
    * Add stylesheet for webcam component
    
    * Add tests for VideoDisplay

---
+++ src/components/WebcamCapture/WebcamCapture.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import './webcamcapture.css';
+import Webcam from 'react-webcam';
+
+const WebcamCapture = () => {
+    return(
+		<Webcam className="videoStream"/>
+	) ;
+}
+
+export default WebcamCapture
---
+++ src/components/WebcamCapture/WebcamCapture.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import WebcamCapture from './webcamcapture.js';
+import {shallow, configure} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import Webcam from 'react-webcam';
+
+configure({ adapter: new Adapter() });
+
+it('It renders a webcam component with class videoStream', () => {
+  const wrapper = shallow(<WebcamCapture />);
+  expect(wrapper.contains(<Webcam className="videoStream" />)).toEqual(true);
+});
