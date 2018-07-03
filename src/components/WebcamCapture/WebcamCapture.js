d32d2bf7b202ec7f457d213e4fc36bbc8bfa8c53
Gurveer Arora <garora1410@yahoo.co.uk>
2018-07-03 11:20:38 +0100

    Display video feed to user

--- src/components/WebcamCapture/WebcamCapture.js
+++ src/components/WebcamCapture/WebcamCapture.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
-import './webcamcapture.css';
+import './WebcamCapture.css';
 import Webcam from 'react-webcam';
 
 const WebcamCapture = () => {
+
+	const videoConstraints = {
+		width: 400,
+		height: 400,
+		facingMode: 'user',
+	  };
+
     return(
-		<Webcam className="videoStream"/>
+		<Webcam className="videoStream" audio={false} height={400} width={400} videoConstraints={videoConstraints} />
 	) ;
 }
 
