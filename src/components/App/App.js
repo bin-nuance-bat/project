7fde477fbe606cccf85fa4aa76d94fe78a0aafe3
Isaac Lapworth <isaacdavidlapworth@gmail.com>
2018-07-03 11:52:13 +0100

    Test prettier

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,10 @@
-import React, { Component } from "react";
-import "./App.css";
+import React, {Component} from 'react';
+import './App.css';
 
 class App extends Component {
-       render() {
-    return <div />;
-  }
+	render() {
+		return <div />;
+	}
 }
 
 export default App;
