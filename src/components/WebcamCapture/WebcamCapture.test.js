8e20e96d717bc9660d0909bbe9d2828dcac699d3
Isaac Lapworth <isaacdavidlapworth@gmail.com>
2018-07-03 13:06:10 +0100

    Prettify existing .js files

--- src/components/WebcamCapture/WebcamCapture.test.js
+++ src/components/WebcamCapture/WebcamCapture.test.js
@@ -1,12 +1,12 @@
-import React from "react";
-import WebcamCapture from "./webcamcapture.js";
-import { shallow, configure } from "enzyme";
-import Adapter from "enzyme-adapter-react-16";
-import Webcam from "react-webcam";
+import React from 'react';
+import WebcamCapture from './webcamcapture.js';
+import {shallow, configure} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import Webcam from 'react-webcam';
 
-configure({ adapter: new Adapter() });
+configure({adapter: new Adapter()});
 
-it("It renders a webcam component with class videoStream", () => {
-  const wrapper = shallow(<WebcamCapture />);
-  expect(wrapper.contains(<Webcam className="videoStream" />)).toEqual(true);
+it('It renders a webcam component with class videoStream', () => {
+	const wrapper = shallow(<WebcamCapture />);
+	expect(wrapper.contains(<Webcam className="videoStream" />)).toEqual(true);
 });
