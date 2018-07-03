dc8b7de03eeab73fb0db6f79403afe2aeb143ccf
Isaac Lapworth <isaacdavidlapworth@gmail.com>
2018-07-03 15:00:15 +0100

    Add loading... screen

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,7 +3,17 @@ import './App.css';
 import WebcamCapture from '../WebcamCapture/WebcamCapture.js';
 
 class App extends Component {
+	state = {
+		loading: true
+	};
+
+	componentDidMount() {
+		this.setState({loading: false});
+	}
+
 	render() {
+		if (this.state.loading) return <div>Loading...</div>;
+
 		return (
 			<div>
 				<WebcamCapture />
