550ba1c3e62b65f60527183e02e15f91c6980176
Isaac Lapworth <isaacdavidlapworth@gmail.com>
2018-07-04 10:55:26 +0100

    basic UI stuff e.g. loading message, checking camera connection, headings (#28)
    * Prettified
    
    * Add loading... screen
    
    * Add headers to page
    
    * Show "cannot access camera" correctly
    
    * Separate Webcam capture into stateful/stateless
    
    * Remove ./IndexList component
    
    * Added confirmation box to UI, not yet functional
    
    * Add circle.yml specifying node version
    
    * Set node version to 8.11.3
    
    * Added null check on navigator.mediaDevices
    
    * Add function to send a direct slack message by username
    
    * Conditionally render confirmation box
    
    * Add function to set ML prediction

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,38 @@
 import React, {Component} from 'react';
 import './App.css';
-import WebcamCapture from '../WebcamCapture/WebcamCapture.js';
+import WebcamCaptureContainer from '../WebcamCapture/WebcamCaptureContainer.js';
+import ConfirmationBox from '../ConfirmationBox/ConfirmationBox';
+import ButtonList from '../ButtonList/ButtonList';
 
 class App extends Component {
+	state = {
+		prediction: null,
+		showList: false
+	};
+
+	setPrediction = label => {
+		if (!this.state.prediction) this.setState({prediction: label});
+	};
+
 	render() {
 		return (
 			<div>
-				<WebcamCapture />
+				<header>
+					<h1> Honesty Store Kiosk</h1>
+					Please take and item and show it to the camera
+				</header>
+				<hr />
+				<WebcamCaptureContainer confirmMatch={this.confirmMatch} />
+				{this.state.prediction && (
+					<ConfirmationBox
+						text={'Did you choose: ' + this.state.prediction + '?'}
+						onYes={() => this.setState({prediction: null})}
+						onNo={() => this.setState({showList: true})}
+					/>
+				)}
+				{this.state.showList && (
+					<ButtonList items={['store', 'item', 'list']} />
+				)}
 			</div>
 		);
 	}
