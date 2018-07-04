184c352ce1e178475b259fbb26f44e2736fe5e13
Isaac Lapworth <isaacdavidlapworth@gmail.com>
2018-07-04 10:49:31 +0100

    Add function to set ML prediction

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,10 +2,16 @@ import React, {Component} from 'react';
 import './App.css';
 import WebcamCaptureContainer from '../WebcamCapture/WebcamCaptureContainer.js';
 import ConfirmationBox from '../ConfirmationBox/ConfirmationBox';
+import ButtonList from '../ButtonList/ButtonList';
 
 class App extends Component {
 	state = {
-		likelyItem: null
+		prediction: null,
+		showList: false
+	};
+
+	setPrediction = label => {
+		if (!this.state.prediction) this.setState({prediction: label});
 	};
 
 	render() {
@@ -16,13 +22,16 @@ class App extends Component {
 					Please take and item and show it to the camera
 				</header>
 				<hr />
-				<WebcamCaptureContainer />
-				{this.state.likelyItem && (
+				<WebcamCaptureContainer confirmMatch={this.confirmMatch} />
+				{this.state.prediction && (
 					<ConfirmationBox
-						text={'Did you choose: ' + this.state.likelyItem + '?'}
-						// onYes={} hide confirmation box
-						// onNo={} prompt user to select correct item
+						text={'Did you choose: ' + this.state.prediction + '?'}
+						onYes={() => this.setState({prediction: null})}
+						onNo={() => this.setState({showList: true})}
 					/>
+				)}
+				{this.state.showList && (
+					<ButtonList items={['store', 'item', 'list']} />
 				)}
 			</div>
 		);
