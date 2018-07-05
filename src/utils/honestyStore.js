9668080b7d53398e8d05c98983944e671c33cce8
William Caine <cainy393@gmail.com>
2018-07-05 17:43:25 +0100

    Use /session endpoint to speed calls up

--- src/utils/honestyStore.js
+++ src/utils/honestyStore.js
@@ -1,10 +1,10 @@
 const getStore = callback => {
-	fetch('https://honesty.store/api/v1/register', {
+	fetch('https://honesty.store/api/v1/session', {
 		method: 'POST',
 		headers: {
-			'Content-Type': 'application/json; charset=utf-8'
-		},
-		body: JSON.stringify({storeCode: 'sl-ncl'})
+			Authorization:
+				'Bearer: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI1NWJjNjY5Yi01OTA4LTQxM2EtOWEyMC0yMGY1MGI2ZjE0NDQiLCJyZWZyZXNoVG9rZW4iOiJmOTYxMzFlNC0wZDBiLTQxOTUtYTViZC1jMWNiODkyYmUyYmMiLCJpYXQiOjE1MzA4MDgwMjksImV4cCI6MTU2MjM2NTYyOX0.cfEF4mNjsCl1af5u31deGz_VSXsmFDxttwb4aw58mmM'
+		}
 	})
 		.then(res => res.json())
 		.then(res => callback(null, res.response.store.items))
