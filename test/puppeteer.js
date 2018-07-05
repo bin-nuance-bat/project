b44c0b4aa29e3f2e2df97a400e96b97fb0bdb88c
Gurveer Arora <garora1410@yahoo.co.uk>
2018-07-05 16:12:58 +0100

    pupeteer

--- test/puppeteer.js
+++ test/puppeteer.js
@@ -1,8 +1,20 @@
+// const puppeteer = require('puppeteer');
+
+// (async () => {
+// 	const browser = await puppeteer.launch();
+// 	const page = await browser.newPage();
+// 	await page.goto("../public/index.html")
+
+// 	await browser.close();
+// })().catch(err => console.log(err));
+
 const puppeteer = require('puppeteer');
 
 (async () => {
 	const browser = await puppeteer.launch();
 	const page = await browser.newPage();
-	// extend
+	await page.goto('http://localhost:3000/');
+	await page.screenshot({path: 'exaple.png'});
+
 	await browser.close();
-})().catch(err => console.log(err));
+})();
