19a0f75aa0e7967ee6022cdfb76070556b9dfae9
William Caine <cainy393@gmail.com>
2018-07-06 11:27:33 +0100

    Obscure token

--- src/utils/honestyStore.js
+++ src/utils/honestyStore.js
@@ -2,8 +2,7 @@ const getStore = callback => {
 	fetch('https://honesty.store/api/v1/session', {
 		method: 'POST',
 		headers: {
-			Authorization:
-				'Bearer: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI1NWJjNjY5Yi01OTA4LTQxM2EtOWEyMC0yMGY1MGI2ZjE0NDQiLCJyZWZyZXNoVG9rZW4iOiJmOTYxMzFlNC0wZDBiLTQxOTUtYTViZC1jMWNiODkyYmUyYmMiLCJpYXQiOjE1MzA4MDgwMjksImV4cCI6MTU2MjM2NTYyOX0.cfEF4mNjsCl1af5u31deGz_VSXsmFDxttwb4aw58mmM'
+			Authorization: 'Bearer: ' + process.env.HONESTY_STORE_TOKEN
 		}
 	})
 		.then(res => res.json())
