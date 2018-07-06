aeb9b775e0745f35f668caf9fe5dfe6404794a3e
Gurveer Arora <garora1410@yahoo.co.uk>
2018-07-06 12:59:18 +0100

    Fix lint error

--- test/puppeteer.js
+++ test/puppeteer.js
@@ -1,21 +1,7 @@
-// const puppeteer = require('puppeteer');
-
-// (async () => {
-// 	const browser = await puppeteer.launch();
-// 	const page = await browser.newPage();
-// 	await page.goto()
-// 	await page.screenshot({path: 'exaple.png'});
-
-// 	await browser.close();
-// })().catch(err => console.log(err));
-
 const puppeteer = require('puppeteer');
 
 (async () => {
-	const browser = await puppeteer.launch({
-		args: ['--use-file-for-fake-video-capture=download.jpg']
-	});
-	const page = await browser.newPage();
-
+	const browser = await puppeteer.launch();
+	// extend
 	await browser.close();
 })();
