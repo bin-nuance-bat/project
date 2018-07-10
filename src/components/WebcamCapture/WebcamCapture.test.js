aaf213539c20a8f856ed4a76c7803dff704fe590
Isaac Lapworth <isaacdavidlapworth@gmail.com>
2018-07-10 16:39:16 +0100

    update webcam tests

--- src/components/WebcamCapture/WebcamCapture.test.js
+++ src/components/WebcamCapture/WebcamCapture.test.js
@@ -6,23 +6,20 @@ import WebcamCapture from './WebcamCapture';
 configure({adapter: new Adapter()});
 
 it("Tells the user that webcam isn't found if not found", () => {
-	const wrapper = shallow(
-		<WebcamCapture cameraConnected={false} cameraRef={{current: null}} />
-	);
+	const wrapper = shallow(<WebcamCapture />);
+	wrapper.setState({isDetecting: false});
 	expect(wrapper.find('ErrorMessage')).toHaveLength(1);
 });
 
 it('Renders webcam with screenshot width of 224', () => {
-	const wrapper = shallow(
-		<WebcamCapture cameraConnected={true} cameraRef={{test: 'value'}} />
-	);
+	const wrapper = shallow(<WebcamCapture />);
+	wrapper.setState({isDetecting: false, cameraConnected: true});
 	expect(wrapper.find('Webcam').props().screenshotWidth).toEqual(224);
 });
 
 it('Renders webcam with screenshot format of jpeg', () => {
-	const wrapper = shallow(
-		<WebcamCapture cameraConnected={true} cameraRef={{test: 'value'}} />
-	);
+	const wrapper = shallow(<WebcamCapture />);
+	wrapper.setState({isDetecting: false, cameraConnected: true});
 	expect(wrapper.find('Webcam').props().screenshotFormat).toEqual(
 		'image/jpeg'
 	);
