bf7b6f370707f138debba67fb16d529b8b67e2e2
Isaac Lapworth <isaacdavidlapworth@gmail.com>
2018-07-11 15:50:52 +0100

    add agree button to disclaimer, no styling

--- src/components/Disclaimer/Disclaimer.js
+++ src/components/Disclaimer/Disclaimer.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
 const Disclaimer = props => {
-	return <div>~disclaimer~</div>;
+	return (
+		<div>
+			~disclaimer~
+			<button onClick={() => props.history.push('/scanitem')}>
+				Agree
+			</button>
+		</div>
+	);
 };
 
 export default Disclaimer;
