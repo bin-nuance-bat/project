fd3965c5e9cb5abb0dc4c6a5bb72a8003bbc5d2d
Isaac Lapworth <isaacdavidlapworth@gmail.com>
2018-07-12 09:15:01 +0100

    add Slack svg

--- src/components/UsernameEntry/UsernameEntry.js
+++ src/components/UsernameEntry/UsernameEntry.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import SlackIcon from './../../res/SlackIcon';
+import './UsernameEntry.css';
 
 class UsernameEntry extends React.Component {
 	componentDidMount() {
@@ -9,13 +11,16 @@ class UsernameEntry extends React.Component {
 	render() {
 		return (
 			<div>
-				Slack Username:
-				<input
-					value={this.props.currentUser}
-					onChange={event =>
-						this.props.setCurrentUser(event.target.value)
-					}
-				/>
+				<SlackIcon size={100} />
+				Your Slack handle is...
+				<div>
+					<input
+						value={this.props.currentUser}
+						onChange={event =>
+							this.props.setCurrentUser(event.target.value)
+						}
+					/>
+				</div>
 			</div>
 		);
 	}
