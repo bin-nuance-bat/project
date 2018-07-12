faeaa3de8b6bd0b096e6d681c1151688af18f937
Gurveer Arora <garora1410@yahoo.co.uk>
2018-07-12 13:09:49 +0100

    test fix

--- src/components/ConfirmationBox/ConfirmationBox.js
+++ src/components/ConfirmationBox/ConfirmationBox.js
@@ -1,61 +1,28 @@
-import React, {Component} from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import {setActualItem} from './actions';
-import {connect} from 'react-redux';
 
-class ConfirmationBox extends Component {
-	handleYes = () => {
-		this.props.setActualItem(
-			this.props.storeList[this.props.prediction.id].name
-		);
-		// go to slack enter
-	};
-
-	handleNo = () => {
-		this.props.history.push('/editSnack');
-	};
-
-	render() {
-		return (
+const ConfirmationBox = props => {
+	return (
+		<div>
+			<div>{`Did you take ${props.item.name}?`}</div>
+			{props.children}
 			<div>
-				<div>{`Did you take ${
-					this.props.storeList[this.props.prediction.id].name
-				}?`}</div>
-				<img src={this.props.prediction.img} alt="" />
-				<div>
-					<button testID="YES" onClick={this.handleYes}>
-						Yes
-					</button>
-					<button testID="NO" onClick={this.handleNo}>
-						No
-					</button>
-				</div>
+				<button testID="YES" onClick={props.onYes}>
+					Yes
+				</button>
+				<button testID="NO" onClick={props.onNo}>
+					No
+				</button>
 			</div>
-		);
-	}
-}
+		</div>
+	);
+};
 
 ConfirmationBox.propTypes = {
-	prediction: PropTypes.object.isRequired,
-	storeList: PropTypes.objectOf(PropTypes.object).isRequired
+	onYes: PropTypes.func.isRequired,
+	onNo: PropTypes.any.isRequired,
+	item: PropTypes.object.isRequired,
+	children: PropTypes.node
 };
 
-const mapStateToProps = state => {
-	return {
-		prediction: state.prediction,
-		storeList: state.storeList
-	};
-};
-
-const mapDispatchToProps = dispatch => {
-	return {
-		setActualItem: itemName => {
-			dispatch(setActualItem(itemName));
-		}
-	};
-};
-
-export default connect(
-	mapStateToProps,
-	mapDispatchToProps
-)(ConfirmationBox);
+export default ConfirmationBox;
