fcd1b02d7023c8329bb4d7a1c0a53b0c0cce22c2
Gurveer Arora <garora1410@yahoo.co.uk>
2018-07-13 12:03:39 +0100

    import and use WebcamCpature

--- src/components/SnackChat/SnackChat.js
+++ src/components/SnackChat/SnackChat.js
@@ -1,76 +1,15 @@
-import React from 'react';
-import Webcam from 'react-webcam';
-import Notification from '../Notification/Notification';
+import React, {Component} from 'react';
+import WebcamCapture from '../WebcamCapture/container';
 
-const height = 400;
-const width = 400;
-
-class SnackChat extends React.Component {
-	state = {
-		isDetecting: true,
-		cameraConnected: false
-	};
-
-	webcam = React.createRef();
-
-	setupScreenshotInterval() {
-		this.ticker = setInterval(() => {
-			const img = new Image(224, 224);
-			img.src = this.webcam.current.getScreenshot();
-
-			img.onload = () => {
-				console.log(img);
-			};
-		}, 500);
-	}
-
-	setupWebcam() {
-		navigator.mediaDevices
-			.getUserMedia({video: true})
-			.then(() => {
-				this.setState({
-					cameraConnected: true,
-					isDetecting: false
-				});
-				this.setupScreenshotInterval();
-			})
-			.catch(() =>
-				this.setState({cameraConnected: false, isDetecting: false})
-			);
-	}
-
-	componentDidMount() {
-		if (!navigator.mediaDevices) return;
-		this.setupWebcam();
-	}
-
-	componentWillUnmount() {
-		clearInterval(this.ticker);
-	}
+class SnackChat extends Component {
+	handleImg = img => {};
 
 	render() {
-		if (this.state.isDetecting) {
-			return null;
-		}
-
-		if (this.state.cameraConnected) {
-			return (
-				<div>
-					<header>Smile for SnackChat</header>
-					<Webcam
-						audio={false}
-						height={height}
-						ref={this.webcam}
-						screenshotFormat="image/jpeg"
-						width={width}
-						className="videoStream"
-						screenshotWidth={224}
-					/>
-				</div>
-			);
-		}
 		return (
-			<Notification message="failed to load video feed" isError={true} />
+			<div>
+				<header>Smile, you are own snackcaht</header>
+				<WebcamCapture onImgLoad={this.handleImg} interval={1000} />
+			</div>
 		);
 	}
 }
