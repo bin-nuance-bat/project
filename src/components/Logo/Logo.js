aa806069d98270933091a527cafbda6e38422a63
Isaac Lapworth <isaacdavidlapworth@gmail.com>
2018-07-13 10:39:15 +0100

    add svgs

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import logo from './logo.svg';
+import './Logo.css';
+
+const Logo = () => (
+	<div>
+		<img src={logo} id="logo" alt="" />
+	</div>
+);
+
+export default Logo;
--- src/components/SuccessPage/SuccessPage.js
+++ src/components/SuccessPage/SuccessPage.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './SuccessPage.css';
+import Logo from './../Logo/Logo';
+import FreddoHand from './../FreddoHand/FreddoHand';
 
 const SuccessPage = props => {
 	return (
 		<div id="page" onClick={() => props.history.push('/')}>
-			Reminder sent!
+			<Logo />
+			<div id="message">Reminder sent!</div>
+			<div id="hand">
+				<FreddoHand />
+			</div>
 		</div>
 	);
 };
