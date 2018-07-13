70d5e64eefd9a838def90bf2fe81818463f87c56
Gurveer Arora <garora1410@yahoo.co.uk>
2018-07-13 09:49:05 +0100

    screenshot

--- src/components/SnackChat/SnackChat.js
+++ src/components/SnackChat/SnackChat.js
@@ -1,36 +1,76 @@
 import React from 'react';
 import Webcam from 'react-webcam';
+import Notification from '../Notification/Notification';
+
+const height = 400;
+const width = 400;
 
 class SnackChat extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {videoSrc: null};
-		window.setTimeout(this.takePhoto, 5000);
+	state = {
+		isDetecting: true,
+		cameraConnected: false
+	};
+
+	webcam = React.createRef();
+
+	setupScreenshotInterval() {
+		this.ticker = setInterval(() => {
+			const img = new Image(224, 224);
+			img.src = this.webcam.current.getScreenshot();
+
+			img.onload = () => {
+				console.log(img);
+			};
+		}, 500);
+	}
+
+	setupWebcam() {
+		navigator.mediaDevices
+			.getUserMedia({video: true})
+			.then(() => {
+				this.setState({
+					cameraConnected: true,
+					isDetecting: false
+				});
+				this.setupScreenshotInterval();
+			})
+			.catch(() =>
+				this.setState({cameraConnected: false, isDetecting: false})
+			);
 	}
 
 	componentDidMount() {
-		if (navigator.getUserMedia) {
-			navigator.getUserMedia(
-				{video: true},
-				v => this.handleVideo(v),
-				function() {}
-			);
-		}
+		if (!navigator.mediaDevices) return;
+		this.setupWebcam();
 	}
 
-	takePhoto() {
-		this.getCanvas();
-	}
-
-	handleVideo(stream) {
-		this.setState({videoSrc: window.URL.createObjectURL(stream)});
+	componentWillUnmount() {
+		clearInterval(this.ticker);
 	}
 
 	render() {
+		if (this.state.isDetecting) {
+			return null;
+		}
+
+		if (this.state.cameraConnected) {
+			return (
+				<div>
+					<header>Smile for SnackChat</header>
+					<Webcam
+						audio={false}
+						height={height}
+						ref={this.webcam}
+						screenshotFormat="image/jpeg"
+						width={width}
+						className="videoStream"
+						screenshotWidth={224}
+					/>
+				</div>
+			);
+		}
 		return (
-			<div>
-				<video src={this.state.videoSrc} autoPlay="true" />
-			</div>
+			<Notification message="failed to load video feed" isError={true} />
 		);
 	}
 }
