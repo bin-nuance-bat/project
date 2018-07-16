b2c485b6349851b2b44d61a5625a6d06960170d9
William Caine <cainy393@gmail.com>
2018-07-16 15:22:19 +0100

    Refactor into smaller components

---
+++ src/components/Admin/Trainer/Settings.js
@@ -0,0 +1,143 @@
+import React, {Component} from 'react';
+
+export default class Settings extends Component {
+	state = {
+		advanced: false
+	};
+
+	render() {
+		return (
+			<div className="col">
+				<div className="formGroup">
+					<label>
+						<input
+							type="checkbox"
+							onChange={() =>
+								this.setState({advanced: !this.state.advanced})
+							}
+							checked={this.state.advanced}
+						/>Show advanced settings
+					</label>
+				</div>
+				{this.state.advanced && (
+					<div className="formGroup">
+						<label>
+							Learning rate:<br />
+							<input
+								type="text"
+								value={this.props.learningRate}
+								onChange={e =>
+									this.props.setState(
+										'learningRate',
+										e.target.value
+									)
+								}
+							/>
+						</label>
+					</div>
+				)}
+				{this.state.advanced && (
+					<div className="formGroup">
+						<label>
+							Batch Size Fraction:<br />
+							<input
+								type="text"
+								value={this.props.batchSizeFraction}
+								onChange={e =>
+									this.props.setState(
+										'batchSizeFraction',
+										e.target.value
+									)
+								}
+							/>
+						</label>
+					</div>
+				)}
+				<div className="formGroup">
+					<label>
+						Epochs:<br />
+						<input
+							type="text"
+							value={this.props.epochs}
+							onChange={e =>
+								this.props.setState('epochs', e.target.value)
+							}
+						/>
+					</label>
+				</div>
+				{this.state.advanced && (
+					<div className="formGroup">
+						<label>
+							Hidden Units:<br />
+							<input
+								type="text"
+								value={this.props.hiddenUnits}
+								onChange={e =>
+									this.props.setState(
+										'hiddenUnits',
+										e.target.value
+									)
+								}
+							/>
+						</label>
+					</div>
+				)}
+				<div className="formGroup">
+					<label>
+						Training Set Size:<br />
+						<input
+							type="text"
+							value={this.props.setSize}
+							onChange={e =>
+								this.props.setState('setSize', e.target.value)
+							}
+						/>
+					</label>
+				</div>
+				<div className="formGroup">
+					<label>
+						Randomness:<br />
+						<input
+							type="text"
+							value={this.props.randomness}
+							onChange={e =>
+								this.props.setState(
+									'randomness',
+									e.target.value
+								)
+							}
+						/>
+					</label>
+				</div>
+				<div className="formGroup">
+					<button
+						onClick={this.props.train}
+						disabled={this.props.busy}>
+						Train
+					</button>
+					<button
+						onClick={this.props.predict}
+						disabled={this.props.busy}>
+						Predict
+					</button>
+					<br />
+					<button
+						onClick={this.props.model.loadModel}
+						disabled={this.props.busy}>
+						Load
+					</button>
+					<button
+						onClick={this.props.model.saveModel}
+						disabled={this.props.busy}>
+						Save
+					</button>
+					<button
+						onClick={this.props.model.exportModel}
+						disabled={this.props.busy}>
+						Export
+					</button>
+				</div>
+			</div>
+		);
+	}
+}
