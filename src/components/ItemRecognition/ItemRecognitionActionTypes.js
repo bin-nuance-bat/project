16bba64a758566e62bbf800ee6b44ebaeaf76d29
Gurveer Arora <Garora1410@yahoo.co.uk>
2018-07-16 13:52:30 +0100

    Refactor webcam capture for snack chat (#86)
    * Allows creation of snackchat paage

---
+++ src/components/ItemRecognition/ItemRecognitionActionTypes.js
@@ -0,0 +1,1 @@
+export const SET_PREDICTION = 'SET_PREDICTION';
--- src/components/ItemRecognition/ItemRecognitionReducer.js
+++ src/components/ItemRecognition/ItemRecognitionReducer.js
@@ -1,4 +1,4 @@
-import {SET_PREDICTION} from './actionTypes';
+import {SET_PREDICTION} from '../ItemRecognition/ItemRecognitionActionTypes';
 
 export function prediction(state = null, action) {
 	if (action.type === SET_PREDICTION) return action.prediction;
