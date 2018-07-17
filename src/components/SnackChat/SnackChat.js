d3903b2960fc2cbcb3e110cabba4319c5c787de9
Isaac Lapworth <isaacdavidlapworth@gmail.com>
2018-07-17 13:56:54 +0100

    Add posenet

--- src/components/SnackChat/SnackChat.js
+++ src/components/SnackChat/SnackChat.js
@@ -1,26 +1,39 @@
 import React, {Component} from 'react';
 import WebcamCapture from '../WebcamCapture/WebcamCaptureContainer';
 import PropTypes from 'prop-types';
+import * as posenet from '@tensorflow-models/posenet';
 
 class SnackChat extends Component {
 	initialTime = new Date();
+
 	constructor(props) {
-    		super(props);
-    		this.state = {counter: 5};
-  }
+		super(props);
+		this.state = {counter: 5};
+	}
+
+	loadPosenet = async () => {
+		this.net = await posenet.load(0.5);
+	};
 
 	componentDidMount() {
 		this.timer = setInterval(this.tick, 1000);
+		this.loadPosenet();
 	}
 
 	tick = () => {
-		this.setState(prevState => ({counter: prevState.counter - 1}));
+		if (this.state.counter > 0)
+			this.setState(prevState => ({counter: prevState.counter - 1}));
+		else clearInterval(this.timer);
 	};
 
-	handleImg = img => {
-		if (this.state.counter <= 0) {
+	handleImg = async img => {
+		if (this.state.counter === 0) {
+			// TODO combine overlay into final image to upload + save in store for later upload
 			this.props.setSnackChat(img.src);
-			this.props.history.push('slackName');
+			this.props.history.push('/slackName');
+		} else {
+			const pose = await this.net.estimateSinglePose(img, 0.5, true, 16);
+			// TODO calculate overlay position + display
 		}
 	};
 
