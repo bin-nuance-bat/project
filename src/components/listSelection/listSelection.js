fd8f28e6624826691635596b65d15a44cabac492
Gurveer Arora <garora1410@yahoo.co.uk>
2018-07-17 14:44:22 +0100

    Sort list alphabetically

--- src/components/listSelection/listSelection.js
+++ src/components/listSelection/listSelection.js
@@ -1,24 +1,36 @@
 import React from 'react';
 import _ from 'lodash';
+import './ListSelection.css';
+
+const compare = (a, b) => {
+	if (a[0] > b[0]) return 1;
+	if (a[0] < b[0]) return -1;
+	return 0;
+};
 
 const getGroupedItems = items => {
-	return _.groupBy(items, item => item.name[0]);
+	const groupList = _.groupBy(items, item => item.name[0]);
+	return Object.entries(groupList).sort(compare);
 };
 
 const ListSelection = ({items}) => {
 	const groupedItems = getGroupedItems(items);
-	return Object.entries(groupedItems).map(([group, groupItems]) => (
-		<div key={group}>
-			<h6>{group.toUpperCase()}</h6>
-			<hr />
-			{groupItems.map(item => (
-				<div key={item.id}>
-					<img src={item.image} className="logo" alt="" />
-					{item.name}
+	return (
+		<div className="listselection">
+			{groupedItems.map(([group, groupItems]) => (
+				<div key={group}>
+					<p className="listtext group">{group.toUpperCase()}</p>
+					<hr />
+					{groupItems.map(item => (
+						<div key={item.id}>
+							<img className="itemicon" src={item.image} alt="" />
+							<p className="listtext item">{item.name}</p>
+						</div>
+					))}
 				</div>
 			))}
 		</div>
-	));
+	);
 };
 
 export default ListSelection;
