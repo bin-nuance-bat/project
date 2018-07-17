66f77de83786ff4909bfb9f86a96a4a552e6523f
Gurveer Arora <garora1410@yahoo.co.uk>
2018-07-17 12:38:06 +0100

    Group list by name and display grouped list

--- src/components/listSelection/listSelection.js
+++ src/components/listSelection/listSelection.js
@@ -1,7 +1,18 @@
-import React, {Component} from 'react';
+import React from 'react';
+import _ from 'lodash';
 
-class listSelection extends Component {
-	render() {}
-}
+const getGroupedItems = items => {
+	return _.groupBy(items, item => item.name[0]);
+};
 
-export default listSelection;
+const ListSelection = ({items}) => {
+	const groupedItems = getGroupedItems(items);
+	return Object.entries(groupedItems).map(([group, groupItems]) => (
+		<div key={group}>
+			<h6>{group.toUpperCase()}</h6>
+			{groupItems.map(item => <div key={item.id}>{item.name}</div>)}
+		</div>
+	));
+};
+
+export default ListSelection;
--- src/components/listSelection/test.js
+++ src/components/listSelection/test.js
@@ -1,10 +1,26 @@
 import React from 'react';
-import listSelection from './listSelection';
+import ListSelection from './listSelection';
 
 const test = () => {
-	list = [];
+	const items = [
+		{id: 'foo', name: 'bar'},
+		{id: 'foo2', name: 'cbar'},
+		{id: 'foo3', name: 'dbar3'},
+		{id: 'foo4', name: 'bar4'},
+		{id: 'foo6', name: 'gar6'},
+		{id: 'foo7', name: 'bar7'},
+		{id: 'foo8', name: 'bar8'},
+		{id: 'foo9', name: 'bar9'},
+		{id: 'foo20', name: 'zbar20'},
+		{id: 'foo10', name: 'bar10'},
+		{id: 'foo11', name: 'bar11'},
+		{id: 'foo12', name: 'bar12'},
+		{id: 'foo18', name: 'bar18'},
+		{id: 'foo19', name: 'bar19'},
+		{id: 'foo5', name: 'bar5'}
+	];
 
-	return <listSelection list={list} />;
+	return <ListSelection items={items} />;
 };
 
 export default test;
