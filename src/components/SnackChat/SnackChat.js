ac02fc637bed5e213c8ab21ef903c285dffdb69c
William Caine <cainy393@gmail.com>
2018-07-19 17:06:09 +0100

    Tidy up code a bit

--- src/components/SnackChat/SnackChat.js
+++ src/components/SnackChat/SnackChat.js
@@ -5,24 +5,38 @@ import * as posenet from '@tensorflow-models/posenet';
 import Logo from '../Logo/Logo';
 import can from '../../assets/coca-cola-can.svg';
 
-function drawEllipse(ctx, centerX, centerY, width, height) {
-	
+function clipEllipse(ctx, centerX, centerY, width, height) {
 	ctx.beginPath();
-	
-	ctx.moveTo(centerX, centerY - height/2); // A1
-	
+	ctx.moveTo(centerX, centerY - height/2);
 	ctx.bezierCurveTo(
-	  centerX + width/2, centerY - height/2, // C1
-	  centerX + width/2, centerY + height/2, // C2
-	  centerX, centerY + height/2); // A2
-  
+		centerX + width/2, centerY - height/2,
+		centerX + width/2, centerY + height/2,
+		centerX, centerY + height/2
+	);
 	ctx.bezierCurveTo(
-	  centerX - width/2, centerY + height/2, // C3
-	  centerX - width/2, centerY - height/2, // C4
-	  centerX, centerY - height/2); // A1
-   
+		centerX - width/2, centerY + height/2,
+		centerX - width/2, centerY - height/2,
+		centerX, centerY - height/2
+	);
 	ctx.clip();
-  }
+}
+
+function normalise({x, y}) {
+	x -= 200;
+	x *= 4/3;
+	x += 200;
+	return {x, y};
+}
+
+function calcAngles(bodyPart) {
+	bodyPart = normalise(bodyPart);
+	bodyPart.width = bodyPart.left.x - bodyPart.right.x;
+	bodyPart.height = bodyPart.right.y - bodyPart.left.y;
+	bodyPart.span = Math.sqrt(bodyPart.width ** 2 + bodyPart.height ** 2);
+	bodyPart.angle = Math.atan(bodyPart.width / bodyPart.height);
+	bodyPart.angle += ((bodyPart.height > 0) ? -1 : 1) * Math.PI / 2;
+	return bodyPart;
+}
 
 class SnackChat extends Component {
 	feedWidth = 400;
@@ -58,8 +72,8 @@ class SnackChat extends Component {
 			clearInterval(this.timer);
 			this.setState({captured: true});
 			this.props.setSnackChat(this.canvas.current.toDataURL());
-			this.props.history.push('/slackname');
-			return;
+			//this.props.history.push('/slackname');
+			//return;
 		}
 		
 		const video = this.webcam.current.webcam.current.video;
@@ -71,67 +85,40 @@ class SnackChat extends Component {
 			16
 		);
 
-		const normalise = ({x, y}) => {
-			x -= video.width / 2;
-			x *= (video.videoWidth / video.width);
-			x += video.width / 2;
-			return {x, y};
-		}
-
 		const body = {
-			ears: {
-				left: normalise(pose.keypoints[3].position),
-				right: normalise(pose.keypoints[4].position)
-			},
-			shoulders: {
-				left: normalise(pose.keypoints[5].position),
-				right: normalise(pose.keypoints[6].position)
-			}
-		}
+			ears: calcAngles({
+				left: pose.keypoints[3].position,
+				right: pose.keypoints[4].position
+			}),
+			shoulders: calcAngles({
+				left: pose.keypoints[5].position,
+				right: pose.keypoints[6].position
+			})
+		};
 
 		// Video background
 		this.ctx.save();
 		this.ctx.scale(-1, 1);
 		this.ctx.drawImage(video, (video.videoWidth - video.width) / 2 - video.videoWidth, -(video.videoHeight - video.height) / 2);
 		this.ctx.restore();
+
 		// Filter
-		const shoulderX = body.shoulders.left.x - body.shoulders.right.x;
-		const shoulderY = body.shoulders.right.y - body.shoulders.left.y;
-		const shoulderGap = Math.sqrt(shoulderX**2 + shoulderY**2);
-		let shoulderAngle = Math.atan(shoulderX / shoulderY);
-
-		shoulderAngle += ((shoulderY > 0) ? -1 : 1) * Math.PI / 2;
-
 		this.ctx.save();
-
-		this.ctx.rotate(shoulderAngle);
-		
+		this.ctx.rotate(body.shoulders.angle);
 		this.ctx.drawImage(
 			this.filter,
-			body.shoulders.right.x - shoulderGap * 1.5 + (shoulderGap * shoulderAngle),
-			body.shoulders.right.y - shoulderGap * 1.5,
-			shoulderGap * 4,
-			shoulderGap * 4
+			body.shoulders.right.x - body.shoulders.span * 1.5 + (body.shoulders.span * body.shoulders.angle),
+			body.shoulders.right.y - body.shoulders.span * 1.5,
+			body.shoulders.span * 4,
+			body.shoulders.span * 4
 		);
-
 		this.ctx.restore();
-
-		// Face calculations
-		const earX = body.ears.left.x - body.ears.right.x;
-		const earY = body.ears.right.y - body.ears.left.y;
-
-		const earGap = Math.sqrt(earX**2 + earY**2);
-		let headAngle = Math.atan(earX / earY);
-
-		headAngle = (earY < 0)
-			? (headAngle + Math.PI / 2)
-			: (headAngle - Math.PI / 2);
 		
-		// Face clip
+		// Clip face
 		this.ctx.save();
-		this.ctx.translate(body.ears.right.x + earX / 2, body.ears.right.y + earY * headAngle);
-		this.ctx.rotate(headAngle);
-		drawEllipse(this.ctx, 0, 0, earGap * 1.3, earGap * 1.3);
+		this.ctx.translate(body.ears.right.x + body.ears.width / 2, body.ears.right.y + body.ears.height * body.ears.angle);
+		this.ctx.rotate(body.ears.angle);
+		clipEllipse(this.ctx, 0, 0, body.ears.span * 1.3, body.ears.span * 1.3);
 		this.ctx.resetTransform();
 
 		// Re-draw face
