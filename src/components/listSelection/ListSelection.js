2413310185a57e4c8478e517115e2c25234eca62
Isaac Lapworth <isaacdavidlapworth@gmail.com>
2018-07-19 14:09:54 +0100

    alternative method of sorting

--- src/components/listSelection/ListSelection.js
+++ src/components/listSelection/ListSelection.js
@@ -3,29 +3,17 @@ import _ from 'lodash';
 import './ListSelection.css';
 import line from './line.svg';
 
-const compareItems = (item1, item2) => {
-	if (item1.name > item2.name) return 1;
-	if (item1.name < item2.name) return -1;
-	return 0;
-};
-
-const compare = (a, b) => {
-	if ((a[0] > b[0] && b[0] !== '#') || a[0] === '#') return 1;
-	if ((a[0] < b[0] && a[0] !== '#') || b[0] === '#') return -1;
-	return 0;
-};
-
-const getGroupedItems = items => {
-	const groupList = _.groupBy(items, item => {
-		const firstLetter = item.name[0];
-		const groupHeading = firstLetter.match(/[a-z]/i) ? firstLetter : '#';
-		return groupHeading;
-	});
-	return Object.entries(groupList).sort(compare);
-};
-
 class ListSelection extends Component {
-	groupedItems = getGroupedItems(this.props.items);
+	formattedItems = (() => {
+		let items = Object.entries(
+			_.groupBy(
+				_.sortBy(this.props.items, 'name'),
+				item => (isNaN(item.name[0]) ? item.name[0] : '#')
+			)
+		);
+		items.push(items.shift());
+		return items;
+	})();
 
 	handleClick = event => {
 		this.props.onClick(event.target.getAttribute('data-key'));
@@ -34,33 +22,34 @@ class ListSelection extends Component {
 	render() {
 		return (
 			<div className="listselection">
-				{this.groupedItems.map(([group, groupItems]) => (
-					<div className="listgroup" key={group}>
-						<p className="listtext grouptext">
-							{group.toUpperCase()}
-						</p>
-						<img src={line} alt="" />
-						{groupItems.sort(compareItems).map(item => (
-							<div
-								className="listitem"
-								key={item.id}
-								data-key={item.id}
-								onClick={this.handleClick}>
-								<img
-									className="itemicon"
-									src={item.image}
-									alt=""
+				{this.props.items.length > 0 &&
+					this.formattedItems.map(([group, groupItems]) => (
+						<div className="listgroup" key={group}>
+							<p className="listtext grouptext">
+								{group.toUpperCase()}
+							</p>
+							<img src={line} alt="" />
+							{groupItems.map(item => (
+								<div
+									className="listitem"
+									key={item.id}
 									data-key={item.id}
-								/>
-								<p
-									data-key={item.id}
-									className="listtext itemtext">
-									{item.name}
-								</p>
-							</div>
-						))}
-					</div>
-				))}
+									onClick={this.handleClick}>
+									<img
+										className="itemicon"
+										src={item.image}
+										alt=""
+										data-key={item.id}
+									/>
+									<p
+										data-key={item.id}
+										className="listtext itemtext">
+										{item.name}
+									</p>
+								</div>
+							))}
+						</div>
+					))}
 			</div>
 		);
 	}
