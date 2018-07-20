6a2f3a53a743b8d8352cfac0079b953edb0f787e
Gurveer Arora <garora1410@yahoo.co.uk>
2018-07-20 11:54:46 +0100

    Create base

---
+++ src/components/TimeoutNotification/TimeoutNotification.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import './TimeoutNotification.css';
+
+class TimeoutNotification extends Component {
+	state = {
+		displayWarning: false,
+		timer: 10
+	};
+
+	render() {
+		return <div> a </div>;
+	}
+}
+
+export default TimeoutNotification;
