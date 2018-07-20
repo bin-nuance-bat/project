8e9964a52f701aa2fb6dfefff01c738a5c051048
Isaac Lapworth <isaacdavidlapworth@gmail.com>
2018-07-20 16:31:22 +0100

    fix travis, move hand to component

---
+++ src/components/Hand/Hand.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import hand from './Hand.svg';
+import './Hand.css';
+
+const Hand = () => (
+	<div className="hand">
+		<img src={hand} alt="" />
+	</div>
+);
+
+export default Hand;
