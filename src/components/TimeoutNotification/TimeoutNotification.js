5bc142fac8eb2ba511d3e1ed5c9f858cc823c1f6
Gurveer Arora <garora1410@yahoo.co.uk>
2018-07-20 17:01:23 +0100

    Add timer and user ineraction

--- src/components/TimeoutNotification/TimeoutNotification.js
+++ src/components/TimeoutNotification/TimeoutNotification.js
@@ -1,32 +1,64 @@
 import React, {Component} from 'react';
 import './TimeoutNotification.css';
+import {Redirect} from 'react-router';
 
-const WAITBEFOREDISPLAY = 1;
+const WAIT_BEFORE_DISPLAY = 2;
+const COUNTDOWN = 5;
 
 class TimeoutNotification extends Component {
 	state = {
 		displayWarning: false,
-		countdown: 10
+		countdown: COUNTDOWN
 	};
 
 	componentDidMount() {
-		this.timer = setTimeout(this.showMessage, WAITBEFOREDISPLAY * 1000);
+		console.log(this.props);
+		document.body.addEventListener('touchstart', () => {
+			this.resetTimer(), this.dismissMessage();
+		});
+		this.resetTimer(0);
 	}
 
 	componentWillUnmount() {
-		clearInterval(this.timer);
+		clearTimeout(this.timer);
 	}
 
-	showMessage = () => {};
+	resetTimer = () => {
+		console.log(22);
+		clearTimeout(this.timer);
+		this.timer = setTimeout(this.showMessage, WAIT_BEFORE_DISPLAY * 1000);
+	};
+
+	showMessage = () => {
+		this.setState({displayWarning: true});
+		clearInterval(this.interval); //WHAT????
+		this.interval = setInterval(this.countdown, 1000);
+	};
+
+	countdown = () => {
+		this.setState(prevState => ({countdown: prevState.countdown - 1}));
+	};
+
+	dismissMessage = () => {
+		this.timer = setTimeout(this.showMessage, WAIT_BEFORE_DISPLAY * 1000);
+		clearInterval(this.interval);
+		this.setState({displayWarning: false, countdown: COUNTDOWN});
+	};
 
 	render() {
+		if (this.state.countdown === 0) {
+			clearInterval(this.interval);
+		}
+
 		return (
-			this.state.displayWarning && (
-				<div className="timeout-notification">
-					{' '}
-					{this.countdown.timer}{' '}
-				</div>
-			)
+			<div>
+				{this.state.displayWarning && (
+					<div className="timeout-notification--notification">
+						{this.state.countdown}
+					</div>
+				)}
+				{this.state.countdown === 0 && <Redirect to="/" />}
+			</div>
 		);
 	}
 }
