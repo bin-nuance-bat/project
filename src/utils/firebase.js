8876769eaedc5242ef8335463614cf3a22763342
Isaac Lapworth <isaacdavidlapworth@gmail.com>
2018-07-23 12:03:19 +0100

    prettier firebase.js

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,14 +1,17 @@
 import firebase from 'firebase/app';
 
 const initFirebase = () => {
-    if (firebase.apps.length === 0) {
-        console.log('Initialising firebase with API key', process.env.REACT_APP_FIREBASE_API_KEY);
-        firebase.initializeApp({
-            apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-            authDomain: 'honesty-store-kiosk.firebaseapp.com',
-            projectId: 'honesty-store-kiosk'
-        });
-    }
-}
+	if (firebase.apps.length === 0) {
+		console.log(
+			'Initialising firebase with API key',
+			process.env.REACT_APP_FIREBASE_API_KEY
+		);
+		firebase.initializeApp({
+			apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+			authDomain: 'honesty-store-kiosk.firebaseapp.com',
+			projectId: 'honesty-store-kiosk'
+		});
+	}
+};
 
 export default initFirebase;
