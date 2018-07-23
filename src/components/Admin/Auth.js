42f7d1bfa342a5e37995c774d97f34a58dcf0696
William Caine <cainy393@gmail.com>
2018-07-23 13:25:42 +0100

    Display message if user is not admin

--- src/components/Admin/Auth.js
+++ src/components/Admin/Auth.js
@@ -20,9 +20,12 @@ class Auth extends Component {
                             name: result.user.displayName,
                             admin: false
                         });
+                    } else if (!uers.data().admin) {
+                        this.setStatus('You are not an administrator.');
+                    } else {
+                        this.props.history.push('/admin');
                     }
                 });
-                this.props.history.push('/admin');
             } else {
                 firebase.auth().signInWithRedirect(new firebase.auth.GoogleAuthProvider());
             }
