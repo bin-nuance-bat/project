1d9a733b64121662f8769a5166983656602d5184
William Caine <cainy393@gmail.com>
2018-07-23 11:37:08 +0100

    Move firebase init to utils

---
+++ src/utils/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase/app';
+
+const initFirebase = () => {
+    if (firebase.apps.length === 0) {
+        console.log('Initialising firebase with API key', process.env.REACT_APP_FIREBASE_API_KEY);
+        firebase.initializeApp({
+            apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+            authDomain: 'honesty-store-kiosk.firebaseapp.com',
+            projectId: 'honesty-store-kiosk'
+        });
+    }
+}
+
+export default initFirebase;
