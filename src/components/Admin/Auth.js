59a40e6846ba59e60bc5b40d87947b9ff4cb8386
William Caine <cainy393@gmail.com>
2018-07-24 12:44:29 +0100

    Fix user auth

--- src/components/Admin/Auth.js
+++ src/components/Admin/Auth.js
@@ -21,23 +21,21 @@ class Auth extends Component {
 						.collection('users')
 						.doc(result.user.uid);
 					userRef.get().then(user => {
-						if (!user.exists) {
-							userRef.set({
-								name: result.user.displayName,
-								admin: false
-							});
-						} else if (!user.data().admin) {
-							this.setStatus('You are not an administrator.');
-						} else {
+						if (user.exists && user.data().admin) {
 							this.props.history.push('/admin');
+						} else {
+							if (!user.exists)
+								userRef.set({
+									name: result.user.displayName,
+									admin: false
+								});
+							this.setState({status: 'You are not an administrator.'});
 						}
 					});
 				} else {
 					firebase
 						.auth()
-						.signInWithRedirect(
-							new firebase.auth.GoogleAuthProvider()
-						);
+						.signInWithRedirect(new firebase.auth.GoogleAuthProvider());
 				}
 			})
 			.catch(function(error) {
