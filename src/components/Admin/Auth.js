9dad7d2316250a959ac0e582d9e23f45ca915faf
William Caine <cainy393@gmail.com>
2018-07-24 10:47:15 +0100

    Fix typo

--- src/components/Admin/Auth.js
+++ src/components/Admin/Auth.js
@@ -5,38 +5,49 @@ import 'firebase/auth';
 import 'firebase/firestore';
 
 class Auth extends Component {
-    state = {
-        status: 'Authenticating...'
-    }
-    
-    componentDidMount() {
-        initFirebase();
-        firebase.auth().getRedirectResult().then(result => {
-            if (result.user) {
-                const userRef = firebase.firestore().collection('users').doc(result.user.uid);
-                userRef.get().then(user => {
-                    if (!user.exists) {
-                        userRef.set({
-                            name: result.user.displayName,
-                            admin: false
-                        });
-                    } else if (!uers.data().admin) {
-                        this.setStatus('You are not an administrator.');
-                    } else {
-                        this.props.history.push('/admin');
-                    }
-                });
-            } else {
-                firebase.auth().signInWithRedirect(new firebase.auth.GoogleAuthProvider());
-            }
-        }).catch(function(error) {
-            console.error(error);
-        });
-    }
+	state = {
+		status: 'Authenticating...'
+	};
 
-    render() {
-        return <h1>{this.state.status}</h1>;
-    }
+	componentDidMount() {
+		initFirebase();
+		firebase
+			.auth()
+			.getRedirectResult()
+			.then(result => {
+				if (result.user) {
+					const userRef = firebase
+						.firestore()
+						.collection('users')
+						.doc(result.user.uid);
+					userRef.get().then(user => {
+						if (!user.exists) {
+							userRef.set({
+								name: result.user.displayName,
+								admin: false
+							});
+						} else if (!user.data().admin) {
+							this.setStatus('You are not an administrator.');
+						} else {
+							this.props.history.push('/admin');
+						}
+					});
+				} else {
+					firebase
+						.auth()
+						.signInWithRedirect(
+							new firebase.auth.GoogleAuthProvider()
+						);
+				}
+			})
+			.catch(function(error) {
+				console.error(error);
+			});
+	}
+
+	render() {
+		return <h1>{this.state.status}</h1>;
+	}
 }
 
 export default Auth;
