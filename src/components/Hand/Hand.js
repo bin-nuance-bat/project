358ba01d5fceff0bd5c5d39145acd34562702117
Isaac Lapworth <isaacdavidlapworth@gmail.com>
2018-07-25 10:53:29 +0100

    improve styling of svgs in-hand

--- src/components/Hand/Hand.js
+++ src/components/Hand/Hand.js
@@ -2,13 +2,70 @@ import React from 'react';
 import handBg from './HandBg.svg';
 import handFg from './HandFg.svg';
 import './Hand.css';
+import PropTypes from 'prop-types';
 
-const Hand = props => (
-  <div className="hand">
-    <img className="hand-component" src={handBg} alt="" />
-    <img className="hand-snack" src={props.snack} alt="" />
-    <img className="hand-component" src={handFg} alt="" />
-  </div>
-);
+const Hand = props => {
+  let itemsToRotate = [
+    'boost',
+    'crunchie',
+    'curly-wurly',
+    'fruit-pastilles-roll',
+    'getbuzzing-mint-choc-high-protein',
+    'getbuzzing-mixed-berries',
+    'kit-kat-chunky',
+    'mars',
+    'mentos',
+    'misc-bar',
+    'nakd-apple-crunch',
+    'nakd-berry-delight',
+    'nakd-cashew-cookie',
+    'nakd-cocoa-orange',
+    'nakd-coconut-bliss-nibbles',
+    'nature-valley-maple-syrup',
+    'nature-valley-oats-dark-chocolate',
+    'nature-valley-oats-n-honey',
+    'panda-liquorice',
+    'skittles',
+    'smarties',
+    'snickers',
+    'trek-banana-bread-flapjack',
+    'trek-peanut-power',
+    'twix',
+    'winegums',
+    'wrigleys'
+  ];
+  let rotate = false;
+  for (let i in itemsToRotate) {
+    if (props.snack.includes(itemsToRotate[i])) {
+      rotate = true;
+      break;
+    }
+  }
+
+  let freddo = props.snack.includes('freddo');
+
+  return (
+    <div className="hand">
+      <img className="hand-component" src={handBg} alt="" />
+      <img
+        className="hand-snack"
+        src={props.snack}
+        style={{
+          transform: freddo
+            ? 'matrix(1, 0, 0, 1, 85, 60)'
+            : `matrix(${
+                rotate ? '0, 1.2, -1.2, 0, 30, 140' : '0.8, 0, 0, 0.8, 30, 30'
+              })`
+        }}
+        alt=""
+      />
+      <img className="hand-component" src={handFg} alt="" />
+    </div>
+  );
+};
+
+Hand.propTypes = {
+  snack: PropTypes.string.isRequired
+};
 
 export default Hand;
