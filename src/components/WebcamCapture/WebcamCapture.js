845367935ec80b7d87ac111af87b96f1a62a095c
Christopher Brown <cbrown@scottlogic.co.uk>
2018-08-01 10:16:50 +0100

    Added  injectWebcam functionality to WebCamCapture.js

--- src/components/WebcamCapture/WebcamCapture.js
+++ src/components/WebcamCapture/WebcamCapture.js
@@ -5,21 +5,56 @@ import PropTypes from 'prop-types';
 import './WebcamCapture.css';
 
 class WebcamCapture extends Component {
+  constructor() {
+    super();
+    global.injectWebcam = ({isDetecting, cameraConnected}) => {
+      this.setState({
+        fakeWebcam: true,
+        isDetecting,
+        cameraConnected
+      });
+      if (this.props.onConnect) this.props.onConnect();
+    };
+  }
+
   state = {
+    fakeWebcam: false,
     isDetecting: true,
     cameraConnected: false
   };
 
   webcam = React.createRef();
 
-  requestScreenshot = () => {
-    if (!this.webcam.current) return Promise.reject('Failed to load webcam.');
-    const screenshot = this.webcam.current.getScreenshot();
-    if (screenshot === null) return Promise.reject('Failed to load webcam.');
+  requestFakeScreenshot = () =>
+    new Promise((resolve, reject) => {
+      const input = this.fileInput;
+      if (input.files == null || input.files[0] == null) {
+        return reject(new Error('No files present'));
+      }
+      const reader = new FileReader();
+      reader.onload = ({target: {result}}) => {
+        resolve(result);
+      };
+      reader.readAsDataURL(input.files[0]);
+    });
 
-    return this.urlToImg(screenshot).then(img =>
-      this.urlToImg(this.cropImage(img))
-    );
+  requestWebcamScreenshot = async () => {
+    if (!this.webcam.current) {
+      throw new Error('Failed to load webcam.');
+    }
+    const screenshot = await this.webcam.current.getScreenshot();
+    if (screenshot === null) {
+      throw new Error('Failed to load webcam.');
+    }
+    return screenshot;
+  };
+
+  requestScreenshot = async () => {
+    const screenshot = this.state.fakeWebcam
+      ? this.requestFakeScreenshot()
+      : this.requestWebcamScreenshot();
+    const image = await this.urlToImg(await screenshot);
+    return await this.urlToImg(this.cropImage(image));
   };
 
   setupWebcam() {
@@ -73,7 +108,13 @@ class WebcamCapture extends Component {
     }
 
     if (this.state.cameraConnected) {
-      return (
+      return this.state.fakeWebcam ? (
+        <input
+          type="file"
+          accept="image/*"
+          ref={fileInput => (this.fileInput = fileInput)}
+        />
+      ) : (
         <div className="webcam-container">
           <Webcam
             audio={false}
