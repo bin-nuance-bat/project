02309e62df7ccb7f287f198e879912bfcfefc862
Isaac Lapworth <isaacdavidlapworth@gmail.com>
2018-08-01 14:22:41 +0100

    import arrow svg

--- src/components/BackButton/BackButton.js
+++ src/components/BackButton/BackButton.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import BackArrow from './BackArrow.svg';
 
 const BackButton = props => {
   return (
@@ -7,16 +8,7 @@ const BackButton = props => {
       <button
         className="button button-back"
         onClick={() => props.history.replace('/')}>
-        <svg width="59" height="61" viewBox="0 0 59 61">
-          <g fill="none" fillRule="evenodd">
-            <polygon points="0 0 59 0 59 59 0 59" />
-            <polygon
-              fill="#FFF"
-              fillRule="nonzero"
-              points="44 10.38 39.602 6 15 30.5 39.602 55 44 50.62 23.797 30.5"
-            />
-          </g>
-        </svg>
+        <img src={BackArrow} alt="" />
       </button>
     </div>
   );
