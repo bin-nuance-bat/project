4916e9cc75278242a5ba1edd413969d013ca3800
Gurveer Arora <garora1410@yahoo.co.uk>
2018-08-01 15:16:34 +0100

    Make into class

--- src/components/SuccessPage/SuccessPage.js
+++ src/components/SuccessPage/SuccessPage.js
@@ -1,29 +1,29 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './SuccessPage.css';
 import Logo from './../Logo/Logo';
 import Hand from './../Hand/Hand';
 import PropTypes from 'prop-types';
 
-const goHomeAndClearTimeout = (props, timeout) => {
-  console.log('aaaa');
-  props.history.replace('/');
-  clearTimeout(timeout);
-};
+class SuccessPage extends Component {
+  timeout = setTimeout(() => this.props.history.replace('/'), 10000);
 
-const SuccessPage = props => {
-  console.log(77);
-  const timeout = setTimeout(props.history.replace('/'), 10000);
-  return (
-    <div className="page" onClick={() => goHomeAndClearTimeout(props, timeout)}>
-      <Logo />
-      <div className="text text-remindersent">Reminder sent!</div>
-      <div className="success-hand">
-        <Hand snack={props.storeList[props.actualItem].image} />
+  goHomeAndClearTimeout = () => {
+    clearTimeout(this.timeout);
+    this.props.history.replace('/');
+  };
+
+  render() {
+    return (
+      <div className="page" onClick={this.goHomeAndClearTimeout}>
+        <Logo />
+        <div className="text text-remindersent">Reminder sent!</div>
+        <div className="success-hand">
+          <Hand snack={this.props.storeList[this.props.actualItem].image} />
+        </div>
       </div>
-    </div>
-  );
-};
-
+    );
+  }
+}
 SuccessPage.propTypes = {
   history: PropTypes.object.isRequired,
   actualItem: PropTypes.string.isRequired,
