21c4d2f4503494bb65deb33e5e417f543af17c16
Gurveer Arora <garora1410@yahoo.co.uk>
2018-08-01 08:46:20 +0100

    Failed

--- src/utils/retry.js
+++ src/utils/retry.js
@@ -1,12 +1,45 @@
-const retry = (func, args, tries = -1, onError) => {
-  func.apply(null, args).catch(error => {
-    onError();
+async function retr(func, args, tries, onError) {
+  try {
+    return func.apply(null, args);
+  } catch (error) {
+    onError && onError();
 
-    if (tries == 1) {
+    console.log('debug', tries);
+
+    if (tries === 1) {
       throw error;
     }
-    return retry(func, args, tries - 1);
-  });
-};
+    setTimeout(() => retr(func, args, tries - 1), 3000);
+  }
+}
 
-export default retry;
+// async function retr(func, args, onError) {
+//   try {
+//     return func.apply(null, args);
+//   } catch (error) {
+//     onError();
+//     const interval = setInterval( () => func.apply(null, args) ,3000)
+//   }
+// }
+
+// class retry {
+//   constructor(func, args, onError, maxTries) {
+//     this.func = func;
+//     this.args = args;
+//     this.onError = onError;
+//     this.maxTries = maxTries;
+//   }
+
+//   start = () => {
+//     return
+//   }
+
+//   try = () => {
+//     try {
+
+//     }
+//   }
+// }
+
+// export default retry;
+export default retr;
