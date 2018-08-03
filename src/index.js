4424937f0b32b94e16e027df7dfaba51060769fd
William Caine <cainy393@gmail.com>
2018-08-03 12:45:25 +0100

    Remove unwanted comments

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 
 import store from './utils/reduxStore';
-import registerServiceWorker from './registerServiceWorker';
 import App from './components/App/AppContainer';
 
 import './index.css';
@@ -14,6 +13,3 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 );
-
-if (window.location.host === 'honesty-store-kiosk.firebaseapp.com')
-  registerServiceWorker();
