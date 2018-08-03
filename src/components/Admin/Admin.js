002f5121c865839c0a9b508bd034d145fd0535b5
William Caine <cainy393@gmail.com>
2018-08-03 13:59:54 +0100

    Improve admin auth

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -1,8 +1,10 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
+
 import initFirebase from '../../utils/firebase';
 import firebase from 'firebase/app';
 import 'firebase/auth';
+
 import './Admin.css';
 
 class Admin extends Component {
@@ -16,43 +18,40 @@ class Admin extends Component {
   };
 
   authenticate = () => {
-    firebase
-      .auth()
-      .getRedirectResult()
-      .then(result => {
-        if (result.user) {
-          const userRef = firebase
-            .firestore()
-            .collection('users')
-            .doc(result.user.uid);
-
-          userRef.get().then(user => {
-            this.setState({loggedIn: true});
-            if (user.exists) {
-              if (user.data().admin) {
-                this.initAdmin(user.data().name);
-              } else {
-                userRef.set({
-                  name: result.user.displayName,
-                  admin: false
-                });
-                this.setState({status: 'You are not an administrator.'});
-              }
-            } else {
-              this.setState({status: 'You are not an administrator.'});
-            }
-          });
-        } else {
-          this.setState({status: 'You are not authenticated.'});
-        }
-      })
-      .catch(function(error) {
-        this.setState({status: error});
-      });
+    firebase.auth().onAuthStateChanged(this.loggedIn);
   };
 
   login = () => {
     firebase.auth().signInWithRedirect(new firebase.auth.GoogleAuthProvider());
+  };
+
+  loggedIn = user => {
+    if (!user) {
+      this.setState({status: 'You are not authenticated'});
+      return;
+    }
+
+    const userRef = firebase
+      .firestore()
+      .collection('users')
+      .doc(user.uid);
+
+    userRef.get().then(u => {
+      this.setState({loggedIn: true});
+      if (u.exists) {
+        if (u.data().admin) {
+          this.initAdmin(u.data().name);
+        } else {
+          this.setState({status: 'You are not an administrator.'});
+        }
+      } else {
+        this.setState({status: 'You are not an administrator.'});
+        userRef.set({
+          name: user.displayName,
+          admin: false
+        });
+      }
+    });
   };
 
   logout = () => {
@@ -117,11 +116,12 @@ class Admin extends Component {
       <div className="page">
         <h1 className="admin-title">Admin Panel</h1>
         <p>{this.state.status}</p>
-        {!this.state.loggedIn && (
-          <button className="button button-admin" onClick={this.login}>
-            Authenticate
-          </button>
-        )}
+        {!this.state.loggedIn &&
+          this.state.status !== 'Loading...' && (
+            <button className="button button-admin" onClick={this.login}>
+              Authenticate
+            </button>
+          )}
         {this.state.admin && (
           <div>
             <div>
