498c407a2129595ae8102045d2032563e2f73d53
Gurveer Arora <Garora1410@yahoo.co.uk>
2018-08-03 14:40:12 +0100

    Move the sending slack message logic to cloud functions (#279)
    * Create firebase functions
    
    * Call function from app
    
    * Move sending slack message to cloud function
    
    * More helpful error messagfe
    
    * Use Constamts for firebase functions
    
    * Update ItemRecognition.js

--- src/components/UsernameEntry/actions.js
+++ src/components/UsernameEntry/actions.js
@@ -5,6 +5,8 @@ import {
 } from './actionTypes';
 
 import retry from '../../utils/retry';
+import firebase from 'firebase/app';
+import 'firebase/functions';
 
 const token = process.env.REACT_APP_SLACK_TOKEN;
 
@@ -54,17 +56,24 @@ export const sendSlackMessage = userid => async (dispatch, getState) => {
 
   try {
     const result = await retry(
-      () =>
-        fetch(`https://slack.com/api/chat.postMessage?token=${token}&
-      channel=${userid}&icon_url=https://honesty.store/assets/android/icon@MDPI.png&username=honesty.store&
-      text=${`Click to purchase your ${itemName}: https://honesty.store/item/${actualItemID}`}`),
+      () => attemptSendSlackMessage(userid, itemName, actualItemID),
       () => dispatch(setSendMessageError(true))
-    ).then(response => {
-      dispatch(setSendMessageError(false));
-      return response.json();
-    });
+    );
+    dispatch(setSendMessageError(false));
+
     return result.ok;
   } catch (error) {
     return false;
   }
+};
+
+const attemptSendSlackMessage = async (userid, itemName, actualItemID) => {
+  const send = firebase.functions().httpsCallable('sendSlackMessage');
+  const result = await send({userid, itemName, actualItemID}).then(response => {
+    return response.data;
+  });
+  if (!result.ok) {
+    throw Error('A network error occured');
+  }
+  return result;
 };
