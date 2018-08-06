1c5a0c56fdefe7a7bb8269aad799d395266334d1
William Caine <cainy393@gmail.com>
2018-08-06 14:46:54 +0100

    Progress

--- functions/index.js
+++ functions/index.js
@@ -1,25 +1,46 @@
 const functions = require('firebase-functions');
 const fetch = require('node-fetch');
-const queryString = require('query-string');
+const toFile = require('data-uri-to-file');
+const request = require('request-promise-native');
 
 const HONESTY_STORE_LOGO = 'https://honesty.store/assets/android/icon@MDPI.png';
 const BOT_USERNAME = 'honesty.store';
 
 exports.sendSlackMessage = functions.https.onCall(data => {
   const token = functions.config().slack.token;
-  const options = queryString.stringify({
-    token,
+  const attachments = data.snackChat
+    ? [
+        {
+          fallback: 'Your SnackChat',
+          image_url: data.snackChat
+        }
+      ]
+    : [];
+
+  const options = {
+    auth: {bearer: token},
     channel: data.userid,
     icon_url: HONESTY_STORE_LOGO,
     username: BOT_USERNAME,
     text: `Click to purchase your ${
       data.itemName
-    }: https://honesty.store/item/${data.actualItemID}`
-  });
+    }: https://honesty.store/item/${data.actualItemID}`,
+    attachments
+  };
 
-  return fetch(`https://slack.com/api/chat.postMessage?${options}`)
-    .then(response => response.json())
-    .catch(() => ({ok: false}));
+  return request.post('https://slack.com/api/chat.postMessage', options);
+});
+
+exports.uploadImageToSlack = functions.https.onCall(data => {
+  const token = functions.config().slack.token;
+  const options = {
+    auth: {bearer: token},
+    form: {
+      channels: data.userid,
+      content: toFile(data.snackChat)
+    }
+  };
+  return request.post('https://slack.com/api/files.upload', options);
 });
 
 exports.loadSlackUsers = functions.https.onCall(() => {
