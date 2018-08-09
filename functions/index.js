8fe5d554622fa81647225b4897876d39c49e4253
Isaac Lapworth <isaacdavidlapworth@gmail.com>
2018-08-09 12:12:11 +0100

    change firebase functions region to europe

--- functions/index.js
+++ functions/index.js
@@ -20,66 +20,72 @@ const authenticateUser = uid => {
     .then(doc => doc.data().admin);
 };
 
-exports.sendSlackMessage = functions.https.onCall((data, context) => {
-  return authenticateUser(context.auth.uid).then(isAllowedAccsess => {
-    if (isAllowedAccsess) {
-      const token = functions.config().slack.token;
+exports.sendSlackMessage = functions
+  .region('europe-west1')
+  .https.onCall((data, context) => {
+    return authenticateUser(context.auth.uid).then(isAllowedAccsess => {
+      if (isAllowedAccsess) {
+        const token = functions.config().slack.token;
 
-      const options = {
-        auth: {bearer: token},
-        json: true,
-        body: {
-          channel: data.userid,
-          as_user: true,
-          text: `Click to purchase your ${
-            data.itemName
-          }: https://honesty.store/item/${data.actualItemID}`
-        }
-      };
+        const options = {
+          auth: {bearer: token},
+          json: true,
+          body: {
+            channel: data.userid,
+            as_user: true,
+            text: `Click to purchase your ${
+              data.itemName
+            }: https://honesty.store/item/${data.actualItemID}`
+          }
+        };
 
-      return request.post('https://slack.com/api/chat.postMessage', options);
-    } else {
-      return ACCESS_DENIED;
-    }
+        return request.post('https://slack.com/api/chat.postMessage', options);
+      } else {
+        return ACCESS_DENIED;
+      }
+    });
   });
-});
 
-exports.sendSnackChat = functions.https.onCall((data, context) => {
-  return authenticateUser(context.auth.uid).then(isAllowedAccsess => {
-    if (isAllowedAccsess) {
-      fs.writeFileSync('/tmp/snackchat.jpg', toBuffer(data.snackChat));
-      const token = functions.config().slack.token;
-      const options = {
-        json: true,
-        formData: {
-          token,
-          channels: data.userid,
-          title: 'Your SnackChat',
-          initial_comment: `Click to purchase your ${
-            data.itemName
-          }: https://honesty.store/item/${data.actualItemID}`,
-          file: fs.createReadStream('/tmp/snackchat.jpg')
-        }
-      };
-      return request.post('https://slack.com/api/files.upload', options);
-    } else {
-      return ACCESS_DENIED;
-    }
+exports.sendSnackChat = functions
+  .region('europe-west1')
+  .https.onCall((data, context) => {
+    return authenticateUser(context.auth.uid).then(isAllowedAccsess => {
+      if (isAllowedAccsess) {
+        fs.writeFileSync('/tmp/snackchat.jpg', toBuffer(data.snackChat));
+        const token = functions.config().slack.token;
+        const options = {
+          json: true,
+          formData: {
+            token,
+            channels: data.userid,
+            title: 'Your SnackChat',
+            initial_comment: `Click to purchase your ${
+              data.itemName
+            }: https://honesty.store/item/${data.actualItemID}`,
+            file: fs.createReadStream('/tmp/snackchat.jpg')
+          }
+        };
+        return request.post('https://slack.com/api/files.upload', options);
+      } else {
+        return ACCESS_DENIED;
+      }
+    });
   });
-});
 
-exports.loadSlackUsers = functions.https.onCall((data, context) => {
-  return authenticateUser(context.auth.uid).then(isAllowedAccsess => {
-    if (isAllowedAccsess) {
-      const token = functions.config().slack.token;
-      const options = {
-        auth: {bearer: token},
-        json: true
-      };
+exports.loadSlackUsers = functions
+  .region('europe-west1')
+  .https.onCall((data, context) => {
+    return authenticateUser(context.auth.uid).then(isAllowedAccsess => {
+      if (isAllowedAccsess) {
+        const token = functions.config().slack.token;
+        const options = {
+          auth: {bearer: token},
+          json: true
+        };
 
-      return request.get('https://slack.com/api/users.list', options);
-    } else {
-      return ACCESS_DENIED;
-    }
+        return request.get('https://slack.com/api/users.list', options);
+      } else {
+        return ACCESS_DENIED;
+      }
+    });
   });
-});
