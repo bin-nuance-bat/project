43e2c07881b53201025de7975571a814bd4a4e3c
William Caine <cainy393@gmail.com>
2018-08-17 10:56:46 +0100

    Reset backend on each model load

--- src/utils/model.js
+++ src/utils/model.js
@@ -2,24 +2,25 @@ import * as tf from '@tensorflow/tfjs';
 import {loadFrozenModel} from '@tensorflow/tfjs-converter';
 import labels from './labels';
 
-import {setTensorTracker} from '@tensorflow/tfjs-core/dist/tensor';
-import {MathBackendWebGL} from '@tensorflow/tfjs-core/dist/kernels/backend_webgl';
-
-const PREPROCESS_DIVISOR = tf.scalar(255 / 2);
+let backendCtor = null;
 
 export default class Model {
   async load() {
-    console.log('Resetting WebGL backend');
+    global.tf = tf;
     tf.ENV.reset();
-    tf.ENV.registerBackend(
-      'webgl',
-      () => new MathBackendWebGL(),
-      2,
-      setTensorTracker
-    );
-    tf.Environment.setBackend('webgl');
+    const backend = tf.ENV.findBackend('webgl');
+    if (backend != null) {
+      backendCtor = backend.__proto__.constructor;
+    } else {
+      tf.ENV.registerBackend(
+        'webgl',
+        () => new backendCtor(),
+        2
+        // setTensorTracker
+      );
+      tf.Environment.setBackend('webgl');
+    }
 
-    console.log('Loading model');
     this.model = await loadFrozenModel(
       '/model/web_model.pb',
       '/model/weights_manifest.json'
@@ -28,18 +29,18 @@ export default class Model {
 
   dispose() {
     if (this.model) this.model.dispose();
-    console.log('Removing backend');
     tf.ENV.removeBackend('webgl');
     tf.ENV.reset();
   }
 
   async predict(element) {
-    if (!this.model) return {value: 0, id: ''};
+    if (!this.model) return [{value: 0, id: ''}];
     const predictions = tf.tidy(() => {
       const input = tf.fromPixels(element);
+      const preProcessDivisor = tf.scalar(255 / 2);
       const preProcessedInput = tf.div(
-        tf.sub(input.asType('float32'), PREPROCESS_DIVISOR),
-        PREPROCESS_DIVISOR
+        tf.sub(input.asType('float32'), preProcessDivisor),
+        preProcessDivisor
       );
 
       const reshapedInput = preProcessedInput.reshape([
