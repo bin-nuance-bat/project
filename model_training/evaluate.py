913418f89b6aa8f1858f26a723e8e10760bc2ebc
William Caine <cainy393@gmail.com>
2018-08-24 14:24:57 +0100

    Tidy up evaluate.py

--- model_training/evaluate.py
+++ model_training/evaluate.py
@@ -14,40 +14,28 @@ import sys
 import threading
 
 
-def read_tensor_from_image_file(file_name,
-                                input_height=299,
-                                input_width=299,
-                                input_mean=0,
-                                input_std=255):
+def load_json(file_name):
+    with open(file_name) as f:
+        file = json.load(f)
+    return file
+
+
+def read_tensor_from_image_file(file_name, input_height=224, input_width=224, input_mean=0, input_std=255):
     input_name = "file_reader"
     file_reader = tf.read_file(file_name, input_name)
-    if file_name.endswith(".png"):
-        image_reader = tf.image.decode_png(
-            file_reader, channels=3, name="png_reader")
-    elif file_name.endswith(".gif"):
-        image_reader = tf.squeeze(
-            tf.image.decode_gif(file_reader, name="gif_reader"))
-    elif file_name.endswith(".bmp"):
-        image_reader = tf.image.decode_bmp(file_reader, name="bmp_reader")
-    else:
-        image_reader = tf.image.decode_jpeg(
-            file_reader, channels=3, name="jpeg_reader")
+    image_reader = tf.image.decode_jpeg(
+        file_reader, channels=3, name="jpeg_reader")
+
     float_caster = tf.cast(image_reader, tf.float32)
     dims_expander = tf.expand_dims(float_caster, 0)
     resized = tf.image.resize_bilinear(
         dims_expander, [input_height, input_width])
     normalized = tf.divide(tf.subtract(resized, [input_mean]), [input_std])
+
     sess = tf.Session()
     result = sess.run(normalized)
 
     return result
-
-
-def load_labels(label_file):
-    label = []
-    with open(label_file) as f:
-        label = json.load(f)
-    return label
 
 
 def path_to_dict(path):
@@ -61,33 +49,63 @@ def path_to_dict(path):
     return d
 
 
-def print_loading_bar(completion, width=60):
+def print_loading_bar(completion, status, width=60):
     done = round(completion * width)
     remaining = width - done
     percentage = round(completion * 100)
+    message = "  {:3}%  {}{} {}".format(
+        percentage, "█" * done, "▒" * remaining, status) + " " * 20
 
-    sys.stdout.write("\b" * (width + 21 + len(str(percentage))))
-    sys.stdout.write("Evaluating model: {}{} {}% ".format(
-        "█" * done, "▒" * remaining, percentage))
+    sys.stdout.write(message)
+    sys.stdout.write("\b" * len(message))
     sys.stdout.flush()
 
 
+class Evaluator:
+    def __init__(self, img_size):
+        self.img_size = img_size
+        self.total_correct_guesses = 0
+        self.total_incorrect_guesses = 0
+        self.labels = load_json("../public/model/labels.json")
+        self.readable_labels = load_json("readable_labels.json")
+
+    def to_label(self, index):
+        return self.labels[index]
+
+    def to_name(self, label):
+        return self.readable_labels[label]
+
+    def test_image(self, sess, label, img, res):
+        file_name = "./eval_data/" + label + "/" + img
+        t = read_tensor_from_image_file(
+            file_name, input_height=self.img_size, input_width=self.img_size)
+
+        results = sess.run("final_result:0", {
+            "Placeholder:0": t
+        })
+
+        results = np.squeeze(results)
+        top_k = results.argsort()[-3:][::-1]
+
+        if (self.to_label(top_k[0]) == label):
+            self.total_correct_guesses += 1
+            res['correct_guesses'] += 1
+            res['ratio'] += (results[top_k[0]] / results[top_k[1]])
+        else:
+            self.total_incorrect_guesses += 1
+            res['incorrect_guesses'] += 1
+            res['incorrect_labels'].append(
+                self.to_name(self.to_label(top_k[0])))
+
+        for i in top_k:
+            if self.to_label(i) == label:
+                res['certainty'] += results[i]
+
+
 if __name__ == "__main__":
-    label_file = "../public/model/labels.json"
-    input_height = 160
-    input_width = 160
-    input_mean = 0
-    input_std = 255
-    input_layer = "Placeholder"
-    output_layer = "final_result"
-
-    labels = load_labels(label_file)
-    readable_labels = load_labels("readable_labels.json")
-
-    correct_guesses = 0
-    incorrect_guesses = 0
     test_data = path_to_dict('./eval_data')['children']
     num_classes = len(test_data)
+
     res_file = open("results.csv", "w")
     res_file.write(
         "Item,Correct Predictions,Mean Certainty,Mean Ratio,Incorrect Labels\n")
@@ -99,54 +117,26 @@ if __name__ == "__main__":
             "model"
         )
 
-        print_loading_bar(0)
+        eval = Evaluator(img_size=160)
+        print("\nEvaluating model...")
+        print_loading_bar(0, "Testing...")
 
         for idx, label in enumerate(test_data):
             if label['type'] != "directory":
                 continue
 
-            certainty = 0
-            guesses = [0, 0]  # correct, incorrect
-            ratio = 0
-            incorrect_labels = []
-
-            def test_image(img):
-                global certainty
-                global ratio
-                global correct_guesses
-                global incorrect_guesses
-
-                file_name = "./eval_data/" + label['name'] + "/" + img['name']
-                t = read_tensor_from_image_file(
-                    file_name,
-                    input_height=input_height,
-                    input_width=input_width,
-                    input_mean=input_mean,
-                    input_std=input_std)
-
-                results = sess.run("final_result:0", {
-                    "Placeholder:0": t
-                })
-                results = np.squeeze(results)
-                top_k = results.argsort()[-3:][::-1]
-
-                if (labels[top_k[0]] == label['name']):
-                    correct_guesses = correct_guesses + 1
-                    guesses[0] = guesses[0] + 1
-                    ratio = ratio + \
-                        (results[top_k[0]] / results[top_k[1]])
-                else:
-                    incorrect_guesses = incorrect_guesses + 1
-                    guesses[1] = guesses[1] + 1
-                    incorrect_labels.append(readable_labels[labels[top_k[0]]])
-
-                for i in top_k:
-                    if labels[i] == label['name']:
-                        certainty = certainty + results[i]
+            results = {
+                'correct_guesses': 0,
+                'incorrect_guesses': 0,
+                'certainty': 0,
+                'ratio': 0,
+                'incorrect_labels': []
+            }
 
             threads = []
             for img in label['children']:
-                t = threading.Thread(target=test_image, args=(img,))
+                t = threading.Thread(target=eval.test_image, args=(
+                    sess, label['name'], img['name'], results,))
                 threads.append(t)
                 t.start()
 
@@ -155,20 +145,23 @@ if __name__ == "__main__":
 
             res_file.write(
                 "{},{}%,{}%,{},{}\n".format(
-                    readable_labels[label['name']],
-                    round(100 * guesses[0] / (guesses[0] + guesses[1])),
-                    round(certainty * 100 / len(label['children']), 2),
-                    round(ratio / len(label['children']), 2),
-                    '"' + ", ".join(incorrect_labels) + '"'
+                    eval.to_name(label['name']),
+                    round(100 * results['correct_guesses'] /
+                          (results['correct_guesses'] + results['incorrect_guesses'])),
+                    round(results['certainty'] * 100 /
+                          len(label['children']), 2),
+                    round(results['ratio'] / len(label['children']), 2),
+                    '"' + ", ".join(results['incorrect_labels']) + '"'
                 )
             )
 
-            print_loading_bar((idx + 1) / num_classes)
+            print_loading_bar((idx + 1) / num_classes,
+                              "Testing {}...".format(eval.to_name(label['name'])))
 
-    sys.stdout.write("\n")
+    sys.stdout.write("\n\n")
 
-    accuracy = 100 * correct_guesses / \
-        (correct_guesses + incorrect_guesses)
+    accuracy = 100 * eval.total_correct_guesses / \
+        (eval.total_correct_guesses + eval.total_incorrect_guesses)
 
     res_file.write(
         "Overall,{}%\n".format(
@@ -178,4 +171,4 @@ if __name__ == "__main__":
     res_file.close()
 
     print("Overall model accuracy: " + str(round(accuracy, 1)) + "%")
-    print("For detailed results see generated results.csv")
+    print("See generated results.csv for more details.")
