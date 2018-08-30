85e45a40a7ab8755c1c358cc610d0015bcec3e68
William Caine <cainy393@gmail.com>
2018-08-30 14:09:46 +0100

    Only send training image if available

--- src/components/UsernameEntry/UsernameEntry.js
+++ src/components/UsernameEntry/UsernameEntry.js
@@ -28,7 +28,7 @@ class UsernameEntry extends React.Component {
   };
 
   sendReminder = () => {
-    if (this.props.dataController)
+    if (this.props.dataController && this.props.capturedImg)
       this.props.dataController.addImage(
         this.props.capturedImg,
         this.props.actualItem
@@ -98,7 +98,7 @@ UsernameEntry.propTypes = {
   sendWithPhoto: PropTypes.bool.isRequired,
   snackChat: PropTypes.object,
   dataController: PropTypes.object.isRequired,
-  capturedImg: PropTypes.string.isRequired
+  capturedImg: PropTypes.string
 };
 
 export default UsernameEntry;
