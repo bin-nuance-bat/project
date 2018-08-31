6ac5c8315f93f267a2ba24b3a6498a667594fce5
William Caine <cainy393@gmail.com>
2018-08-31 17:10:30 +0100

    Still wip

--- src/components/SnackChat/FilterView.js
+++ src/components/SnackChat/FilterView.js
@@ -1,36 +1,206 @@
 import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 import {Container, Sprite} from '@inlet/react-pixi';
 import * as PIXI from 'pixi.js';
 
+const PIVOT = new PIXI.Point(50, 50);
+const POSE_BUFFER_LEN = 10;
+
 class FilterView extends Component {
   state = {
-    rotation: 0
+    mask: null,
+    ...getBlankPose()
   };
 
+  filter = new PIXI.Texture.fromImage(this.props.image);
+  graphics = new PIXI.Graphics();
+  poseBuffer = [];
+  poseBufferIndex = 0;
+
   componentDidMount() {
-    this.props.app.ticker.add(this.tick);
+    this.updatePose();
+    for (let i = 0; i < POSE_BUFFER_LEN; i++) {
+      this.poseBuffer.push({
+        filterSize: 0,
+        filterX: 0,
+        filterY: 0,
+        faceSpan: 0,
+        faceX: 0,
+        faceY: 0,
+        rotation: 0
+      });
+    }
   }
 
-  componentWillUnmount() {
-    this.props.app.ticker.remove(this.tick);
-  }
+  getAveragePose = pose => {
+    const filterSize = pose.shoulders.span * 3;
 
-  tick = delta => {
-    this.setState(({rotation}) => ({rotation: rotation + 0.1 * delta}));
+    const filterX =
+      pose.shoulders.leftX +
+      Math.abs((pose.shoulders.leftX - pose.shoulders.rightX) / 2);
+    const filterY =
+      pose.shoulders.leftY +
+      Math.abs((pose.shoulders.leftY - pose.shoulders.rightY) / 2);
+
+    const faceX =
+      pose.ears.leftX + Math.abs((pose.ears.leftX - pose.ears.rightX) / 2);
+    const faceY =
+      pose.ears.leftY + Math.abs((pose.ears.leftY - pose.ears.rightY) / 2);
+    const faceSpan = pose.ears.span * 0.75;
+
+    const rotation = pose.shoulders.angle;
+
+    const newPose = {
+      filterSize,
+      filterX,
+      filterY,
+      faceX,
+      faceY,
+      faceSpan,
+      rotation
+    };
+    this.poseBuffer[this.poseBufferIndex++] = newPose;
+
+    const avgPose = getBlankPose();
+
+    for (const key in Object.keys(newPose)) {
+      for (let i = 0; i < POSE_BUFFER_LEN; i++) {
+        avgPose[key] *= i;
+        avgPose[key] += this.poseBuffer[i];
+        avgPose[key] /= i + 1;
+      }
+    }
+
+    return avgPose;
+  };
+
+  updatePose = () => {
+    this.props.getPose().then(pose => {
+      if (!pose) return requestAnimationFrame(this.updatePose);
+
+      const {width, height} = this.props.app.screen;
+      const {
+        filterSize,
+        filterX,
+        filterY,
+        faceX,
+        faceY,
+        faceSpan,
+        rotation
+      } = this.getAveragePose(pose);
+
+      const mask = new PIXI.Graphics()
+        .beginFill(0, 0.5)
+        .moveTo(0, 0)
+        .lineTo(0, height)
+        .lineTo(width, height)
+        .lineTo(width, 0)
+        .lineTo(0, 0)
+        .lineTo(faceX, faceY - faceSpan)
+        .bezierCurveTo(
+          faceX + faceSpan / 2,
+          faceY - faceSpan,
+          faceX + faceSpan,
+          faceY - faceSpan / 2,
+          faceX + faceSpan,
+          faceY
+        )
+        .bezierCurveTo(
+          faceX + faceSpan,
+          faceY + faceSpan / 2,
+          faceX + faceSpan / 2,
+          faceY + faceSpan,
+          faceX,
+          faceY + faceSpan
+        )
+        .bezierCurveTo(
+          faceX - faceSpan / 2,
+          faceY + faceSpan,
+          faceX - faceSpan,
+          faceY + faceSpan / 2,
+          faceX - faceSpan,
+          faceY
+        )
+        .bezierCurveTo(
+          faceX - faceSpan,
+          faceY - faceSpan / 2,
+          faceX - faceSpan / 2,
+          faceY - faceSpan,
+          faceX,
+          faceY - faceSpan
+        )
+        .lineTo(0, 0);
+
+      if (this.state.mask) this.state.mask.destroy();
+
+      this.setState({
+        filterSize,
+        filterX,
+        filterY,
+        mask,
+        rotation
+      });
+
+      console.log(pose.ears, pose.shoulders);
+
+      requestAnimationFrame(this.updatePose);
+    });
   };
 
   render() {
     return (
       <Container>
-        <Sprite
-          image={this.props.image}
-          x={200}
-          y={200}
-          rotation={this.state.rotation}
-        />
+        {this.state.pose && (
+          // <Graphics
+          //   draw={g => {
+          //     g.clear().lineStyle(3, 0xff0000, 1);
+          //     g.moveTo(this.state.pose.ears.leftX, this.state.pose.ears.leftY);
+          //     g.lineTo(
+          //       this.state.pose.ears.rightX,
+          //       this.state.pose.ears.rightY
+          //     );
+          //     g.moveTo(
+          //       this.state.pose.shoulders.leftX,
+          //       this.state.pose.shoulders.leftY
+          //     );
+          //     g.lineTo(
+          //       this.state.pose.shoulders.rightX,
+          //       this.state.pose.shoulders.rightY
+          //     );
+          //   }}
+          // />
+          <Sprite
+            texture={this.filter}
+            x={this.state.filterX}
+            y={this.state.filterY}
+            width={this.state.filterSize}
+            height={this.state.filterSize}
+            rotation={this.state.rotation}
+            pivot={PIVOT}
+            mask={this.state.mask}
+          />
+        )}
       </Container>
     );
   }
 }
+
+function getBlankPose() {
+  return {
+    filterSize: 0,
+    filterX: 0,
+    filterY: 0,
+    rotation: 0,
+    faceX: 0,
+    faceY: 0,
+    faceSpan: 0
+  };
+}
+
+FilterView.propTypes = {
+  app: PropTypes.object.isRequired,
+  getPose: PropTypes.func.isRequired,
+  image: PropTypes.string.isRequired
+};
 
 export default FilterView;
--- src/components/SnackChat/SnackChat.js
+++ src/components/SnackChat/SnackChat.js
@@ -3,10 +3,27 @@ import PropTypes from 'prop-types';
 import {Stage, Provider} from '@inlet/react-pixi';
 
 import FilterView from './FilterView';
+import WebcamCapture from '../WebcamCapture/WebcamCapture';
 import BackButton from '../BackButton/BackButton';
 
 import * as posenet from '@tensorflow-models/posenet';
 import './SnackChat.css';
+
+const FEED_SIZE = 768;
+const CAPTURE_SIZE = 200;
+
+function calcAngles(bodyPart) {
+  bodyPart.width = bodyPart.leftX - bodyPart.rightX;
+  bodyPart.height = bodyPart.rightY - bodyPart.leftY;
+  bodyPart.span = Math.sqrt(bodyPart.width ** 2 + bodyPart.height ** 2);
+  bodyPart.angle = Math.atan(bodyPart.width / bodyPart.height);
+  bodyPart.angle += ((bodyPart.height > 0 ? -1 : 1) * Math.PI) / 2 + Math.PI;
+  return bodyPart;
+}
+
+function normalise(n) {
+  return (n *= FEED_SIZE / CAPTURE_SIZE);
+}
 
 class SnackChat extends Component {
   state = {
@@ -14,11 +31,52 @@ class SnackChat extends Component {
     rotation: 0
   };
 
+  webcamCap = React.createRef();
+
   componentDidMount() {
+    posenet.load(0.5).then(net => (this.net = net));
     this.props.loadStoreList().then(() => {
       this.setState({loading: false});
     });
+    window.scaleFactor = 0.3;
   }
+
+  getPose = async () => {
+    if (!this.net) return null;
+
+    let frame;
+
+    try {
+      frame = await this.webcamCap.current.requestScreenshot();
+    } catch (e) {
+      //console.error(e);
+      return null;
+    }
+
+    const pose = await this.net.estimateSinglePose(
+      frame,
+      window.scaleFactor,
+      true,
+      16
+    );
+
+    const body = {
+      ears: calcAngles({
+        leftX: normalise(pose.keypoints[4].position.x),
+        leftY: normalise(pose.keypoints[4].position.y),
+        rightX: normalise(pose.keypoints[3].position.x),
+        rightY: normalise(pose.keypoints[3].position.y)
+      }),
+      shoulders: calcAngles({
+        leftX: normalise(pose.keypoints[6].position.x),
+        leftY: normalise(pose.keypoints[6].position.y),
+        rightX: normalise(pose.keypoints[5].position.x),
+        rightY: normalise(pose.keypoints[5].position.y)
+      })
+    };
+
+    return body;
+  };
 
   render() {
     return (
@@ -28,17 +86,28 @@ class SnackChat extends Component {
           <div className="header-text">SnackChat</div>
         </header>
         <div>
+          <WebcamCapture
+            imgSize={CAPTURE_SIZE}
+            onFail={() => {}}
+            ref={this.webcamCap}
+          />
           {!this.state.loading && (
-            <Stage>
+            <Stage
+              width={FEED_SIZE}
+              height={FEED_SIZE}
+              options={{transparent: true}}
+              className="snackchat-stage">
               <Provider>
                 {app => (
                   <FilterView
                     image={
                       this.props.storeList[
-                        'ccad58e3-e27a-4463-9139-17a36ff7f7b8'
+                        '606e12d4-6367-4fc3-aa7a-92ee17ccac2c'
                       ].image
                     }
                     app={app}
+                    getPose={this.getPose}
+                    video={this.webcamCap.current.webcam.current.video}
                   />
                 )}
               </Provider>
